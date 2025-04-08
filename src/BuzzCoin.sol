8cb9b3581f5195a830c9972a195879f2c9282f19
Abchee, Alexandre Gaby <aabchee3@gatech.edu>
2025-04-08 14:46:42 -0400

    old version

--- src/BuzzCoin.sol
+++ src/BuzzCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BuzzCoin is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Abchee", "BUZZ") {
+    constructor(uint256 initialSupply) ERC20("BuzzCoin", "BUZZ") {
         _mint(msg.sender, initialSupply);
     }
 }
